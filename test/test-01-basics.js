ab2e579a954472e0fa6e284e71ec03e349a3ca98
C J Silverio <ceej@npmjs.com>
2014-04-02 21:08:01 -0700

    Outline the server tests I need to write.

--- test/test-01-basics.js
+++ test/test-01-basics.js
@@ -3,10 +3,16 @@
 var
     lab      = require('lab'),
     describe = lab.describe,
-    it       = lab.it
+    it       = lab.it,
+    demand   = require('must')
     ;
 
 describe('server', function()
 {
-    it('has tests');
+    it('can be constructed');
+    it('can be configured');
+    it('responds on /ping');
+    it('responds on /message');
+    it('posts responses to the provided webhook');
+    it('does not leak messages or other objects per request');
 });
