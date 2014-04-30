18e0b1741b49b1b3cfe479c8fd81abe7699f3693
bcoe <bencoe@gmail.com>
2014-04-30 14:40:14 -0700

    added bot for performing ansible deployments.

---
+++ plugins/deployer.js
@@ -0,0 +1,64 @@
+/*
+Perform deployments with Ansible, and robots!
+*/
+
+var _ = require('lodash'),
+  spawn = require('child_process').spawn;
+
+var Deployer = module.exports = function Deployer(opts) {
+  _.extend(this, {
+    ansibleFolder: '/home/ubuntu/npm-ansible',
+    playbook: './playbooks/deploy-www.yml'
+  }, opts);
+};
+
+Deployer.prototype.name = 'deployer';
+Deployer.prototype.pattern = /deployer\s+(\w+)$/;
+
+Deployer.prototype.matches = function matches(msg)
+{
+  return this.pattern.test(msg);
+};
+
+Deployer.prototype.respond = function respond(message)
+{
+  var msg = message.text,
+    matches = this.pattern.exec(msg);
+
+  if (!matches) {
+    message.done(this.help());
+    return;
+  }
+
+  var environment = matches[1];
+
+  this.execute(environment, message);
+};
+
+Deployer.prototype.help = function help(msg)
+{
+    return 'Deploy www for a specific environment\n' +
+        '`deployer [environment]` - deploy to the enviornment given\n';
+};
+
+Deployer.prototype.execute = function execute(environment, message)
+{
+  var spawn = require('child_process').spawn,
+    ansible = spawn('ansible-playbook', [this.playbook, '/usr', '-i', environment], {
+      cwd: this.ansibleFolder
+    });
+
+  ansible.stdout.on('data', function (data) {
+    message.send(data.toString());
+    console.log('stdout: ' + data);
+  });
+
+  ansible.stderr.on('data', function (data) {
+    message.send(data.toString());
+    console.log('stdout: ' + data);
+  });
+
+  ansible.on('close', function (code) {
+    message.done("finished deploying www", environment);
+  });
+};
