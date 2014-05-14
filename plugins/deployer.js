302844bc84676aa103b061f100f5ce1fac2d4eaf
C J Silverio <ceej@npmjs.com>
2014-05-13 21:48:10 -0700

    Major changes to the deployer plugin:
    - The plugin is now configurable. You can now specify the path to your ansible
      playbook executable, the directory where your ansible data lives, and a mapping
      of app names to playbooks.
    
    - Took the first step toward making the app/playbook combo controllable by
      making the app the first parameter to `execute()`.
    
    - Cleaned up the chat output considerably. The bot now only echoes successful
      tasks and other major steps in the run. It provides slightly more detail on
      fatal errors.
    
    - Switched from `buffer` to gnu `stdbuf` for unbuffering ansible output.
      Instructions for installing stdbuf with homebrew on OS X are in the
      comments in the file header.

--- plugins/deployer.js
+++ plugins/deployer.js
@@ -1,16 +1,37 @@
 /*
-Perform deployments with Ansible, and robots!
+    Perform deployments with Ansible, and robots!
+
+    config options:
+    {
+        ansible: '/path/to/ansible-playbook',
+        configdir: '/path/to/ansible-data',
+        playbooks:
+        {
+            'app-name': './playbooks/app-name.yml',
+            'another':  './playbooks/deploy-another.yml'
+        }
+    }
+
+    For OS X testing, get stdbuf this way:
+    brew tap paulp/extras
+    brew install --HEAD stdbuf
 */
-var _ = require('lodash'),
+
+var
+    _     = require('lodash'),
+    path  = require('path'),
     spawn = require('child_process').spawn;
 
-var Deployer = module.exports = function Deployer(opts) {
-    _.extend(this, {
-        ansibleFolder: '/home/ubuntu/npm-ansible',
-        playbook: './playbooks/deploy-www.yml'
-    }, opts);
+var PATTERNS =
+{
+    PLAY:  /PLAY \[(.*)\]/,
+    TASK:  /TASK:\s+\[.* \|\s?(.*)\]/,
+    FACTS: /GATHERING FACTS/,
+};
 
-    if (!this.spawn) this.spawn = require('child_process').spawn;
+var Deployer = module.exports = function Deployer(opts)
+{
+    _.extend(this, opts);
 };
 
 Deployer.prototype.name = 'deployer';
@@ -31,7 +52,7 @@ Deployer.prototype.respond = function respond(message)
         return;
     }
 
-    this.execute(matches[1], message);
+    this.execute('www', matches[1], message);
 };
 
 Deployer.prototype.help = function help(msg)
@@ -40,27 +61,83 @@ Deployer.prototype.help = function help(msg)
         '`deploy [environment]` - deploy to the environment given\n';
 };
 
-Deployer.prototype.execute = function execute(environment, message)
+Deployer.prototype.execute = function execute(app, environment, message)
 {
-    var ansible = this.spawn('unbuffer', ['ansible-playbook', this.playbook, '-i', environment], {
-        cwd: this.ansibleFolder
-    });
+    var playbook = this.playbooks[app];
 
-    message.send("deploying www to " + environment);
+    var ansible = spawn('stdbuf',
+            ['-o0', this.ansible, playbook, '-i', environment],
+            { cwd: this.configdir });
 
-    ansible.stdout.on('data', function(data) {
+    var accumulator = [];
+    var error = false;
+    var last;
+    var interesting = /(TASK|GATHERING FACTS|PLAY \[)/;
+
+    var tag = app + ' ➜ ' + environment;
+    message.send('Deploying ' + tag);
+
+    ansible.stdout.on('data', function(data)
+    {
         var output = data.toString();
+        accumulator.push(output);
 
-        if (output.indexOf('***') > -1) {
-            message.send(output.replace(/\*/g, ''));
+        if (!interesting.test(output))
+        {
+            if (!last) return;
+            if (/ok: /.test(output))
+                message.send(tag + ': ' + last);
+            if (/fatal: /.test(output))
+            {
+                error = true;
+                message.send(tag + ' fatal error\nTask: ' + last + '\n' + output);
+            }
+
+            last = null;
+            return;
+        }
+
+        var matches;
+
+        matches = output.match(PATTERNS.PLAY);
+        if (matches)
+        {
+            last = 'executing play ' + matches[1];
+            return;
+        }
+
+        matches = output.match(PATTERNS.TASK);
+        if (matches)
+        {
+            last = matches[1];
+            return;
+        }
+
+        matches = output.match(PATTERNS.FACTS);
+        if (matches)
+        {
+            last = 'fact-finding';
+            return;
         }
     });
 
-    ansible.stderr.on('data', function(data) {
+    ansible.stderr.on('data', function(data)
+    {
         message.send(data.toString());
     });
 
-    ansible.on('close', function(code) {
-        message.done("finished deploying www", environment);
+    ansible.on('close', function(code)
+    {
+        setTimeout(function()
+        {
+            if (error)
+            {
+                accumulator.unshift('```');
+                accumulator.push('```');
+                message.send(accumulator.join(''));
+            }
+            message.send(tag + ': complete.');
+            message.done();
+        }, 500);
     });
 };
